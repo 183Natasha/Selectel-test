1e922bb6d74a02cc4795680e69c88f700cb2bfc6
183Natasha <168049595+183Natasha@users.noreply.github.com>
2026-03-07 12:21:51 +0500

    создана стартовая страница

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { StartPage } from './startPage/starPage';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, StartPage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
